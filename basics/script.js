a243b246422026bf6a4a2d7838927bbbaa26b1db
Stephanie Courtney <scourtn4@ie.ibm.com>
2017-04-19 15:19:05 +0100

    switches

---
+++ basics/script.js
@@ -0,0 +1,17 @@
+
+// switches
+var job = 'teacher';
+
+switch(job) {
+    case 'teacher':
+        console.log('Teacher');
+        break;
+    case 'driver':
+        console.log('Driver');
+        break;
+    case 'cop':
+        console.log('Cop');
+        break;
+    default:
+        console.log('Something else');
+}
