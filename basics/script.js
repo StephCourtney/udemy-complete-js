7c97cfcd6a4fe5d17d26bb7b2915ceb1c8db87a0
Steph Courtney <scourtn4@ie.ibm.com>
2017-04-25 08:15:49 +0100

    finished basics

--- basics/script.js
+++ basics/script.js
@@ -15,3 +15,21 @@ switch(job) {
     default:
         console.log('Something else');
 }
+
+// Challange 1
+var p1_height = 100;
+var p2_height = 96;
+var p1_age = 10;
+var p2_age = 11;
+
+var p1_total = p1_height + (p1_age*5);
+var p2_total = p2_height + (p2_age*5);
+
+if(p1_total > p2_total) {
+    console.log('Player 1 wins');
+} else if( p1_total === p2_total) {
+    console.log('Draw');
+}
+else {
+    console.log('Player 2 wins');
+}
